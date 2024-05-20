a13b7108f7d81ce56d5c67b5715d2e6af0d0bb37
williams bolu <williams.bolu99@gmail.com>
2024-05-20 21:19:55 +0100

    changed server logic

---
+++ delete/server.js
@@ -0,0 +1,61 @@
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+
+// process.on('uncaughtException', (err) => {
+//     console.log('UNCAUGHT EXCEPTION! 💥 shutting down...');
+//     // console.log(err.name, err.message);
+//     console.log(err);
+
+//     process.exit(1); // terminate/exit d server
+// });
+
+// dotenv.config({
+//     path: './config.env',
+// });
+
+// const app = require('./app');
+
+// // Gets the database connection string and replace the password
+// const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+// const connectDB = async () => {
+//     try {
+//         const conn = await mongoose.connect(DB);
+//         console.log(`MongoDB Connected: ${conn.connection.host}`);
+//     } catch (error) {
+//         console.log(error);
+//         process.exit(1);
+//     }
+// };
+
+// // console.log(process.env.NODE_ENV);
+
+// const PORT = process.env.PORT || 3000;
+
+// connectDB().then(() => {
+//     const server = app.listen(PORT, () => {
+//         console.log('listening for requests');
+//     });
+
+//     // Handling Unhandled Rejections (promise) for (async) code // for errors outside express (eg: mongodb database connection)
+//     process.on('unhandledRejection', (err) => {
+//         console.log('UNHANDLED REJECTION! 💥 shutting down...');
+//         console.log(err.name, err.message);
+
+//         // finishes all current and pending request and close
+//         server.close(() => {
+//             process.exit(1); // terminate/exit d serer
+//         });
+//     });
+
+//     process.on('SIGTERM', () => {
+//         console.log('✋ SIGTERM RECEIVED. Shutting down gracefully');
+
+//         // finishes all current and pending request and close
+//         server.close(() => {
+//             console.log('💥 Process terminated!');
+//         });
+
+//         // process.exit(1) // we dont need this cuz SIGTERM automatically cause d application to shut down
+//     });
+// });
--- server.js
+++ server.js
@@ -18,44 +18,36 @@ const app = require('./app');
 // Gets the database connection string and replace the password
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(DB);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-};
+mongoose
+    .connect(DB)
+    .then((con) => console.log('DB connection successful!'))
+    .catch((err) => console.log(err));
 
 // console.log(process.env.NODE_ENV);
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log('listening for requests');
+});
 
-connectDB().then(() => {
-    const server = app.listen(PORT, () => {
-        console.log('listening for requests');
+// Handling Unhandled Rejections (promise) for (async) code // for errors outside express (eg: mongodb database connection)
+process.on('unhandledRejection', (err) => {
+    console.log('UNHANDLED REJECTION! 💥 shutting down...');
+    console.log(err.name, err.message);
+
+    // finishes all current and pending request and close
+    server.close(() => {
+        process.exit(1); // terminate/exit d serer
+    });
+});
+
+process.on('SIGTERM', () => {
+    console.log('✋ SIGTERM RECEIVED. Shutting down gracefully');
+
+    // finishes all current and pending request and close
+    server.close(() => {
+        console.log('💥 Process terminated!');
     });
 
-    // Handling Unhandled Rejections (promise) for (async) code // for errors outside express (eg: mongodb database connection)
-    process.on('unhandledRejection', (err) => {
-        console.log('UNHANDLED REJECTION! 💥 shutting down...');
-        console.log(err.name, err.message);
-
-        // finishes all current and pending request and close
-        server.close(() => {
-            process.exit(1); // terminate/exit d serer
-        });
-    });
-
-    process.on('SIGTERM', () => {
-        console.log('✋ SIGTERM RECEIVED. Shutting down gracefully');
-
-        // finishes all current and pending request and close
-        server.close(() => {
-            console.log('💥 Process terminated!');
-        });
-
-        // process.exit(1) // we dont need this cuz SIGTERM automatically cause d application to shut down
-    });
+    // process.exit(1) // we dont need this cuz SIGTERM automatically cause d application to shut down
 });
